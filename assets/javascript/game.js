8af5ffa665ec0af220318dfe2d70d4a48e0a1413
unknown <norman.capers@gmail.com>
2019-06-04 18:03:47 -0400

    updates to css javascript and html

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+console.log("App Starting");
+
+$(document).ready(function() {
+    var wins = 0;
+    var losses = 0;
+    var computerScore = "";
+    var random = 5;
+
+for (var i = 0; i < 9; i++) {
+
+    var random =  Math.floor(Math.random() * 100);
+    
+ }   
+ console.log(random) 
+$("#computerScore-text").append("<h3><hr><center>"+ random + "<center><hr></h3>");
+});
+
+
+
+
+console.log("App Ending");
